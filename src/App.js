1e43f20351a022a50d3a2f001d2447b0833c0346
PetxCode <brighterdayscodelab@gmail.com>
2022-06-07 17:19:08 +0100

    latest push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NewPassword from "./components/Auth/NewPassword";
+import Register from "./components/Auth/Register";
+import ResetPassword from "./components/Auth/ResetPassword";
+import SignIn from "./components/Auth/Signin";
 import Header from "./components/Header/Header";
 import HomeScreen from "./components/Home/HomeScreen";
 import DetailScreen from "./components/Profile/DetailScreen";
@@ -11,6 +15,10 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<HomeScreen />} />
 				<Route path="/detail" element={<DetailScreen />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/signin" element={<SignIn />} />
+				<Route path="/requestReset" element={<ResetPassword />} />
+				<Route path="/newPassword" element={<NewPassword />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ src/components/Auth/NewPassword.js
@@ -0,0 +1,233 @@
+import React from "react";
+import pix from "./babe.jpeg";
+import styled from "styled-components";
+import { FaFacebookSquare, FaRegUserCircle } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import { GiPadlock } from "react-icons/gi";
+import { BsFillPersonFill } from "react-icons/bs";
+import { MdPassword } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const NewPassword = () => {
+	return (
+		<Container>
+			<Wrapper>
+				<Logo>Social Build</Logo>
+
+				<Text>Sign up to see photos and videos from your friends.</Text>
+
+				<Button>
+					<Icon />
+					<span>Log in with Facebook</span>
+				</Button>
+
+				<LineHolder>
+					<Line />
+					<span>or</span>
+					<Line1 />
+				</LineHolder>
+
+				<InputHolder>
+					<Icon1 />
+					<Input placeholder="Password" />
+				</InputHolder>
+
+				<InputHolder>
+					<Icon1 />
+					<Input placeholder="Confirm Password" />
+				</InputHolder>
+
+				<Button1>
+					<Icon6 />
+					<span>Password Reset</span>
+				</Button1>
+			</Wrapper>
+			<Wrapper>
+				<Linked>
+					Don't an account? <Span to="/register">Register</Span>
+				</Linked>
+			</Wrapper>
+		</Container>
+	);
+};
+
+export default NewPassword;
+
+const Button1 = styled.div`
+	background-color: rgb(16, 143, 233);
+	/* width: 100%; */
+	color: white;
+	margin: 20px 0px;
+	padding: 7px 50px;
+	border-radius: 3px;
+	display: flex;
+	align-items: center;
+	font-size: 14px;
+	cursor: pointer;
+`;
+
+const Linked = styled.div`
+	display: flex;
+`;
+const Span = styled(Link)`
+	color: rgba(16, 143, 233);
+	cursor: pointer;
+	margin-left: 5px;
+	text-decoration: none;
+`;
+
+const Icon2 = styled(BsFillPersonFill)`
+	margin: 0 10px;
+	color: gray;
+`;
+const Icon3 = styled(FaRegUserCircle)`
+	margin: 0 10px;
+	color: gray;
+`;
+const Icon4 = styled(MdPassword)`
+	margin: 0 10px;
+	color: gray;
+`;
+const Icon6 = styled(GiPadlock)`
+	margin-right: 10px;
+`;
+
+const Input = styled.input`
+	flex: 1;
+	outline: none;
+	border: 0;
+
+	::placeholder {
+		font-family: Poppins;
+	}
+`;
+
+const Icon1 = styled(AiOutlineMail)`
+	margin: 0 10px;
+	color: gray;
+`;
+
+const InputHolder = styled.div`
+	margin-top: 10px;
+	border: 1px solid silver;
+	width: 80%;
+	height: 35px;
+	display: flex;
+	align-items: center;
+	border-radius: 3px;
+`;
+
+const ImageInput = styled.input`
+	display: none;
+`;
+
+const ImageLabel = styled.label`
+	font-size: 12px;
+	padding: 8px 20px;
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px,
+		rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
+
+	margin: 10px 0;
+	background-color: rgb(16, 143, 233);
+	color: white;
+	border-radius: 3px;
+`;
+
+const Image = styled.img`
+	width: 50px;
+	height: 50px;
+	object-fit: cover;
+	border-radius: 50%;
+	border: 3px solid transparent;
+	outline: 2px solid purple;
+	background-clip: content-box;
+`;
+
+const ImageHold = styled.div`
+	margin-top: 20px;
+	margin-left: 40px;
+	margin-right: 40px;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+`;
+
+const Line1 = styled.div`
+	border-top: 1px solid silver;
+	width: 100%;
+	margin-right: 40px;
+`;
+
+const Line = styled.div`
+	border-top: 1px solid silver;
+	width: 100%;
+	margin-left: 40px;
+`;
+
+const LineHolder = styled.div`
+	display: flex;
+	width: 100%;
+	align-items: center;
+
+	span {
+		margin: 0 10px;
+		text-transform: uppercase;
+		font-size: 12px;
+	}
+`;
+
+const Icon = styled(FaFacebookSquare)`
+	margin-right: 10px;
+`;
+
+const Button = styled.div`
+	background-color: rgb(16, 143, 233);
+	/* width: 100%; */
+	color: white;
+	margin: 20px 0px;
+	padding: 7px 50px;
+	border-radius: 3px;
+	display: flex;
+	align-items: center;
+	font-size: 14px;
+	cursor: pointer;
+`;
+
+const Text = styled.div`
+	padding: 0 30px;
+	color: gray;
+	text-align: center;
+	line-height: 1.2;
+	font-size: 14px;
+`;
+
+const Logo = styled.div`
+	font-family: Pacifico;
+	margin-top: 50px;
+	font-size: 35px;
+	font-weight: 500;
+	margin-bottom: 10px;
+`;
+
+const Wrapper = styled.div`
+	width: 350px;
+	height: 100%;
+	min-height: 100px;
+	box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
+		rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+	margin-top: 30px;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	margin-bottom: 5px;
+	justify-content: center;
+`;
+
+const Container = styled.div`
+	padding-top: 70px;
+	display: flex;
+	width: 100%;
+	align-items: center;
+	flex-direction: column;
+	padding-bottom: 30px;
+`;
